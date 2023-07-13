2d566a9c38eafeba8fbf81d37d23a153ee21f1e1
EunJung803 <aimee3322@gmail.com>
2023-07-14 01:10:19 +0900

    Programmers / [lv 1] - 2016년 (주석 추가)

--- Programmers/lv1_2016년.py
+++ Programmers/lv1_2016년.py
@@ -24,11 +24,12 @@
 def solution(a, b):
     answer = ''
 
-    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
-    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']     # 1/1이 FRI부터 시작했으니 인덱스 1이 FRI인 요일 순서의 리스트
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]    # 각 월이 가지고 있는 총 일 수를 담은 리스트
 
-    total_days = sum(month[:a - 1], b)
-    answer += day[total_days % 7]
+    total_days = sum(month[:a - 1], b)      # a월 b일 의 총 일 수
+    answer += day[total_days % 7]           # a월 b일 이 가지고 있는 총 일 수를 구한 뒤, 총 일 수를 통해 요일을 찾을 수 있다.
+                                            # 일주일 7일을 기반으로 반복되는 요일 리스트 day의 인덱스를 기반으로 7로 나눴을 때의 나머지를 통해 a월 b일의 요일을 찾는다.
 
     return answer
 
