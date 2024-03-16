be648654c707f02ce14c390c4a233e65561037f4
EunJung803 <aimee3322@gmail.com>
2024-03-16 18:40:50 +0900

    Programmers / [lv 2] - 타겟 넘버 (240316 BFS 풀이 추가)

--- Programmers/Lv2/dfs_bfs_타겟넘버.py
+++ Programmers/Lv2/dfs_bfs_타겟넘버.py
@@ -17,20 +17,43 @@ def solution(numbers, target):
     return answer
 
 ### BFS
-# def solution(numbers, target):
-#     answer = 0
-#     queue = [[numbers[0],0], [-1*numbers[0],0]]
-#     n = len(numbers)
-#     while queue:
-#         temp, idx = queue.pop()
-#         idx += 1
-#         if idx < n:
-#             queue.append([temp+numbers[idx], idx])
-#             queue.append([temp-numbers[idx], idx])
-#         else:
-#             if temp == target:
-#                 answer += 1
-#     return answer
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+
+### BFS (240316 풀이)
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+
+    q = deque()
+    q.append([0, 0])
+
+    while (q):
+        curr = q.popleft()
+        idx = curr[0]
+        curr_sum = curr[1]
+
+        if (idx < len(numbers)):
+            q.append([idx + 1, curr_sum + numbers[idx]])
+            q.append([idx + 1, curr_sum - numbers[idx]])
+
+        if (idx == len(numbers) and curr_sum == target):
+            answer += 1
+
+    return answer
 
 if __name__ == '__main__':
     print(solution([1,1,1,1,1], 3))
