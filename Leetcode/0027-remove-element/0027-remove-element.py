24ddf5602a18a4b28110b51750f25d4e64a68725
EunJung Kim <62376361+EunJung803@users.noreply.github.com>
2024-04-17 02:25:40 +0900

    Time: 18 ms (45.36%), Space: 11.6 MB (40.13%) - LeetHub

---
+++ Leetcode/0027-remove-element/0027-remove-element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
+        n = nums.count(val)
+        for i in range(n):
+            nums.remove(val)
+
+        k = len(nums)
+        return k
